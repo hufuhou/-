b08a56b71008a0cccb56044982cb4c2bbb201396
Zero-0007 <762110248@qq.com>
2023-12-05 20:13:18 +0800

    订单模块

---
+++ ruoyi-modules/cx-order/src/main/java/com/cx/order/service/impl/OrderPurchaseServiceImpl.java
@@ -0,0 +1,96 @@
+package com.cx.order.service.impl;
+
+import java.util.List;
+import com.ruoyi.common.core.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cx.order.mapper.OrderPurchaseMapper;
+import com.cx.order.domain.OrderPurchase;
+import com.cx.order.service.IOrderPurchaseService;
+
+/**
+ * 进货订单Service业务层处理
+ * 
+ * @author 刘志昂
+ * @date 2023-11-20
+ */
+@Service
+public class OrderPurchaseServiceImpl implements IOrderPurchaseService 
+{
+    @Autowired
+    private OrderPurchaseMapper orderPurchaseMapper;
+
+    /**
+     * 查询进货订单
+     * 
+     * @param poId 进货订单主键
+     * @return 进货订单
+     */
+    @Override
+    public OrderPurchase selectOrderPurchaseByPoId(Long poId)
+    {
+        return orderPurchaseMapper.selectOrderPurchaseByPoId(poId);
+    }
+
+    /**
+     * 查询进货订单列表
+     * 
+     * @param orderPurchase 进货订单
+     * @return 进货订单
+     */
+    @Override
+    public List<OrderPurchase> selectOrderPurchaseList(OrderPurchase orderPurchase)
+    {
+        return orderPurchaseMapper.selectOrderPurchaseList(orderPurchase);
+    }
+
+    /**
+     * 新增进货订单
+     * 
+     * @param orderPurchase 进货订单
+     * @return 结果
+     */
+    @Override
+    public int insertOrderPurchase(OrderPurchase orderPurchase)
+    {
+        orderPurchase.setCreateTime(DateUtils.getNowDate());
+        return orderPurchaseMapper.insertOrderPurchase(orderPurchase);
+    }
+
+    /**
+     * 修改进货订单
+     * 
+     * @param orderPurchase 进货订单
+     * @return 结果
+     */
+    @Override
+    public int updateOrderPurchase(OrderPurchase orderPurchase)
+    {
+        orderPurchase.setUpdateTime(DateUtils.getNowDate());
+        return orderPurchaseMapper.updateOrderPurchase(orderPurchase);
+    }
+
+    /**
+     * 批量删除进货订单
+     * 
+     * @param poIds 需要删除的进货订单主键
+     * @return 结果
+     */
+    @Override
+    public int deleteOrderPurchaseByPoIds(Long[] poIds)
+    {
+        return orderPurchaseMapper.deleteOrderPurchaseByPoIds(poIds);
+    }
+
+    /**
+     * 删除进货订单信息
+     * 
+     * @param poId 进货订单主键
+     * @return 结果
+     */
+    @Override
+    public int deleteOrderPurchaseByPoId(Long poId)
+    {
+        return orderPurchaseMapper.deleteOrderPurchaseByPoId(poId);
+    }
+}
